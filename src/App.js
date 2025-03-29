4b1bc8420d3c6e7d7eb5bc74370bf361ed874f2e
Nguyễn Đình Trưng <nguyendinhtrung.it@gmail.com>
2025-03-29 21:16:19 +0700

    update delete loading App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Routers } from "./routers";
 import axiosClient from './api/axiosClient.ts';
-import LoadingScreen from './components/loading/loading.js';
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -10,7 +9,7 @@ export default function App() {
     // Simulate data loading or API initialization
     const initializeApp = async () => {
       try {
-        await axiosClient.get('/city');
+        await axiosClient.get('/companies');
         
         await new Promise(resolve => setTimeout(resolve, 1500));
       } catch (error) {
@@ -22,10 +21,6 @@ export default function App() {
 
     initializeApp();
   }, []);
-
-  if (isLoading) {
-    return <LoadingScreen />;
-  }
 
   return <Routers />;
 }
